5a4a2192665e6397488f49c41b675119f355d0f5
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-05-20 12:03:33 -0400

    feat: remake ui and class selection

--- menus/login/login_menu.gd
+++ menus/login/login_menu.gd
@@ -1,10 +1,9 @@
 extends Control
 
-@onready var username: LineEdit = $VBoxContainer/Username
-@onready var pasword: LineEdit = $VBoxContainer/Pasword
-@onready var login_btn: Button = $VBoxContainer/Login
-@onready var switch_register_btn: Button = $VBoxContainer/SwitchRegister
-@onready var http_request: HTTPRequest = $HTTPRequest
+@onready var username: LineEdit = $Center/LoginContainer/Username
+@onready var pasword: LineEdit = $Center/LoginContainer/Pasword
+@onready var login_btn: Button = $Center/LoginContainer/Login
+@onready var switch_register_btn: Button = $Center/LoginContainer/SwitchRegister
 
 func _ready() -> void:
 	var args = Array(OS.get_cmdline_args())
@@ -18,23 +17,24 @@ func _ready() -> void:
 	
 	login_btn.pressed.connect(login)
 	switch_register_btn.pressed.connect(func(): get_tree().change_scene_to_file("res://menus/register/register_menu.tscn"))
-	http_request.request_completed.connect(request_completed)
-	
-func request_completed(result, response_code, headers, body):
-	if response_code != 200: return
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	var file = FileAccess.open(Networking.SESSION_ID_FILE, FileAccess.WRITE)
-	var id = json["session_id"]
-	file.store_string(id)
-	Networking.session_id = id
-	get_tree().change_scene_to_file("res://menus/main/main_menu.tscn")
 
 func login():
-	var data = {
+	var resp = await (Networking.http
+		.http_post("/auth/login")
+		.json({
 		"username": username.text,
 		"password": pasword.text,
-	}
+	})
+		.send())
 	
-	var json = JSON.stringify(data)
-	var headers = ["Content-Type: application/json"]
-	http_request.request(Networking.API_URL + "/auth/login", headers, HTTPClient.METHOD_POST, json)
+	if resp.status() != 200:
+		return
+		
+	var res = await resp.json()
+	var id = res["session_id"]
+	Networking.session_id = id
+	
+	var file = FileAccess.open(Networking.SESSION_ID_FILE, FileAccess.WRITE)
+	file.store_string(id)
+	
+	get_tree().change_scene_to_file("res://menus/main/main_menu.tscn")
